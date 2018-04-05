a263abb5780231071418270509bf1e040da52c11
vavaish <32906746+vavaish@users.noreply.github.com>
2018-04-05 14:17:24 +0300

    Update Main.c

--- Replace/Mhm/Main.c
+++ Replace/Mhm/Main.c
@@ -3,44 +3,41 @@
 #include "string.h"
 #include "stdlib.h"
 #define SIZE 256
-
+int GetKey();
 
 int main()
 {
 	int pos=0;
 	int ch;
 	char string[SIZE]=" ";
-	int i = 0;
-	int len = 0;
-
+	
 	while (1)
 	{
 		ch = _getch();
-		if ((ch>='0')&&(ch<='9'))
+		if ((ch >= '0') && (ch <= '9'))
 		{
 			string[pos] = ch;
 			_putch(string[pos]);
 			pos++;
-			
 		}
-		if ((pos>=10)|| (ch == '\r'))
+		if ((pos >= 10) || (ch == '\r'))
 		{
 			break;
 		}
-	
 		if ((ch == 8) && (pos > 0))
 		{
 			_putch('\b');
 			_putch(' ');
 			_putch('\b');
 			pos--;
+			string[pos] = '\0';
 		}
-		if (ch==75)
+		if (ch == 75 || ch == 224)
 		{
 			_putch('\b');
 			pos--;
 		}
-		if (ch==77)
+		if (ch == 77||ch==224)
 		{
 			_putch(string[pos]);
 			pos++;
@@ -49,5 +46,16 @@ int main()
 	printf("\n%s",string);
 	string[pos] = '\0';
 	system("pause");
+	GetKey();
 	return 0;
+}
+int GetKey()
+{
+	int ch;
+	ch = _getch();
+	if (ch == 0 || ch == 224) {
+		ch = _getch();
+		ch += 256;
+	}
+	return ch;
 }
